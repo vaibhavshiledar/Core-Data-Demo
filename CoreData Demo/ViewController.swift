ea1a415ffac186137c29f6f063d9bcd7bbdf8bd6
admin <vaibhavshiledar@gmail.com>
2020-09-09 21:28:15 +0530

    base project

---
+++ CoreData Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreData Demo
+//
+//  Created by admin on 09/09/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
